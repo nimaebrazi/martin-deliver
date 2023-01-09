a5acb87f0ab9f74c34ff3103148253258bc1af0d
Nima Ebrazi <nima.ebrazi1373@gmail.com>
2023-01-09 07:37:03 +0330

    FIX: http method.

--- routes/api/parcel-v1.php
+++ routes/api/parcel-v1.php
@@ -6,7 +6,7 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::controller(ParcelController::class)->group(function () {
-    Route::get('/register', 'register');
+    Route::post('/register', 'register');
 });
 
 
