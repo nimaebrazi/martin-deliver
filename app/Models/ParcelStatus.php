55f4d04560856bb68a65f5429171e565d648a070
Nima Ebrazi <nima.ebrazi1373@gmail.com>
2023-01-09 07:33:13 +0330

    ADD: parcel models.

---
+++ app/Models/ParcelStatus.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+
+/**
+ * @method static active
+ * @method static parcelId
+ */
+class ParcelStatus extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['status', 'parcel_id', 'is_active'];
+
+    public function parcel()
+    {
+        return $this->belongsTo(Parcel::class, 'parcel_id');
+    }
+
+    public function scopeActive(Builder $query): Builder
+    {
+        return $query->where('is_active', true);
+    }
+
+    public function scopeParcelId(Builder $query, int $parcelId): Builder
+    {
+        return $query->where('parcel_id', '=', $parcelId);
+    }
+}
