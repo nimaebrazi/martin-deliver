3f07174ac2c689c452432e50687c70fda8114be0
Nima Ebrazi <nima.ebrazi1373@gmail.com>
2023-01-10 18:09:46 +0330

    Update drivers-v1.php

--- routes/api/drivers-v1.php
+++ routes/api/drivers-v1.php
@@ -1,6 +1,13 @@
 <?php
 
 
+use App\Http\Controllers\Api\V1\DriverController;
 use Illuminate\Support\Facades\Route;
 
+
+Route::controller(DriverController::class)->middleware('api.token.driver')->group(function () {
+    Route::post('/accept/parcel/{id}', 'accept');
+    Route::put('/cancel/parcel/{id}', 'cancel');
+    Route::put('/status/parcel/{id}', 'updateParcelStatus');
+});
 
